70fc6702126112ce62da0ccf3479c7a51fbecd2c
tvanlong <longvan1173@gmail.com>
2024-10-07 20:45:54 +0700

    redeploy webhook

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,15 +1,8 @@
 import { CourseGrid } from '@/components/common';
 import CourseItem from '@/components/course/CourseItem';
 import Heading from '@/components/typography/Heading';
-import createUser from '@/lib/actions/user.actions';
 
 const page = async () => {
-  const user = await createUser({
-    clerkId: 'clerkId_123',
-    username: 'long',
-    email_address: 'long@gmail.com',
-  });
-
   return (
     <>
       <Heading>Khám phá</Heading>
